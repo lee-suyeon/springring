3eb3ff819bdf01f342f6b504d4dbf7ec35ac6843
leesuyeon <sroovy@naver.com>
2022-05-27 13:35:59 +0900

    AppConfig 리팩터링

--- src/main/java/Hello/core/order/AppConfig.java
+++ src/main/java/Hello/core/order/AppConfig.java
@@ -1,6 +1,8 @@
 package Hello.core.order;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.member.MemberRepository;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
 import Hello.core.member.MemoryMemberRepository;
@@ -8,10 +10,18 @@ import Hello.core.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
