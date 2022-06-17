59722a33bf6076706dbf5427ad5254a4fe7d57f4
leesuyeon <sroovy@naver.com>
2022-06-17 16:22:58 +0900

    싱글톤 컨테이너

---
+++ src/test/java/Hello/core/singleton/StatefulService.java
@@ -0,0 +1,11 @@
+package Hello.core.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+}
