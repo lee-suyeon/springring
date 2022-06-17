933eecf044f81ba97706949b190978afcdf14113
leesuyeon <sroovy@naver.com>
2022-06-17 15:08:38 +0900

    싱글톤 컨테이너

--- src/test/java/Hello/core/singleton/SingletonTest.java
+++ src/test/java/Hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import Hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -33,5 +35,16 @@ public class SingletonTest {
         SingletonService singletonService2 = SingletonService.getInstance();
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
